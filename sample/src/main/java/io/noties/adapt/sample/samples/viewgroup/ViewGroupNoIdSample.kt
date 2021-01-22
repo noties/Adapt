de50b120a5476ef7e0d6e8390d74413c391b0b9c
Dimitry Ivanov <di@noties.io>
2021-01-22 17:52:20 +0300

    sample, viewgroup no id

--- sample/src/main/java/io/noties/adapt/sample/samples/viewgroup/ViewGroupNoIdSample.kt
+++ sample/src/main/java/io/noties/adapt/sample/samples/viewgroup/ViewGroupNoIdSample.kt
@@ -1,19 +1,26 @@
 package io.noties.adapt.sample.samples.viewgroup
 
 import android.view.View
+import android.view.ViewGroup
+import io.noties.adapt.sample.R
 import io.noties.adapt.sample.SampleView
 import io.noties.adapt.sample.annotation.AdaptSample
+import io.noties.adapt.sample.items.NoIdItem
+import io.noties.adapt.viewgroup.AdaptViewGroup
 
 @AdaptSample(
     "20210122143249",
     "NO_ID in ViewGroup",
+    "Indicates that an Item without ID (<tt>NO_ID</tt>) is recreated with each update"
 )
 class ViewGroupNoIdSample : SampleView() {
 
     override val layoutResId: Int
-        get() = TODO("Not yet implemented")
+        get() = R.layout.view_sample_view_group
 
     override fun render(view: View) {
-        TODO("Not yet implemented")
+        val viewGroup = view.findViewById<ViewGroup>(R.id.view_group)
+        val adapt = AdaptViewGroup.init(viewGroup)
+        adapt.setItems(initialItems(adapt).toMutableList().apply { add(NoIdItem()) })
     }
 }
