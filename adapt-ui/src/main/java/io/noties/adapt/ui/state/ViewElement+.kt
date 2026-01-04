0a8db480242334e63a3af3f21f6818e727b98576
Dimitry Ivanov <git@noties.io>
2026-01-04 15:14:26 +0100

    feat: remove deprecations

--- adapt-ui/src/main/java/io/noties/adapt/ui/state/ViewElement+.kt
+++ adapt-ui/src/main/java/io/noties/adapt/ui/state/ViewElement+.kt
@@ -55,8 +55,7 @@ fun <V : ProgressBar, LP : LayoutParams> ViewElement<V, LP>.progressTintWithStat
 
 /**
  * `pressed` is not reported without a stateful drawable for the `android.R.attr.state_pressed`, as
- * a workaround a [io.noties.adapt.ui.shape.StatefulShape], [StateListDrawable] can be used with
- * pressed state defined. Or a stateful drawable
+ * a workaround [StateListDrawable] can be used with pressed state defined. Or a stateful drawable
  * with this state defined, for example, `backgroundDefaultSelectable()` or `foregroundDefaultSelectable()`.
  * Additionally, a regular shape can be turned into stateful drawable with `shape.newDrawable().stateful()`
  */
